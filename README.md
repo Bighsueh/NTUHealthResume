e9de32a876d43499949c8b973ed976d09348c21c
Bighsueh <40841121@gm.nfu.edu.tw>
2021-12-19 21:27:58 +0800

    migration及controller設定

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 台大健康履歷網  
 
+## 系統架構
+* 前端：  
+    1. CSS: Bootstrap  
+    2. Javascript: 原生js,Jquery  
+    3. Laravel Blade  
+
+* 後端：  
+    1. PHP-Laravel  
+    2. DataBase-MySQL  
+
+## 資料庫設計
+* Table Lists
+    1. 
+    
+
+## Controller
+1. LoginController 登入相關功能
+2. DoctorController 醫師操作相關功能
+3. NotificationController 回饋函相關功能
+
--- database/migrations/2021_12_19_124422_create_patitent_replies_table.php
+++ database/migrations/2021_12_19_124422_create_patitent_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePatitentRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('patitent_replies', function (Blueprint $table) {
+            $table->id('patitent_replies_id');
+            $table->string('patitent_replies_patitent_id');
+            $table->text('patitent_replies_text');
+            $table->string('patitent_replies_title');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('patitent_replies');
     }
 }
