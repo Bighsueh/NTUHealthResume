024be0050f262be5c40e827fcd3ef589a60413a0
Bighsueh <40841121@gm.nfu.edu.tw>
2021-12-21 08:48:11 +0800

    加入測試路徑 name('menu')

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 //取得登入介面
 Route::get('/',[LoginController::class,'get_login_page'])->name('get_login_page');
 Route::post('/login',[LoginController::class,'post_login_data'])->name('post_login_data');
+
+
+Route::get('/menu', function () {
+    return view('pages.menu.menu');
+})->name('menu');
