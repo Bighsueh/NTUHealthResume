c8b76b9f9fed136d0eeccad6803c32a374a433af
yoyo <ya90312@gmail.com>
2022-01-04 02:10:43 +0800

    完成登入跳轉功能

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -6,6 +6,26 @@ use Illuminate\Http\Request;
 
 class DriveController extends Controller
 {
+    public function get_drive_login_page()
+    {
+        return view('pages.drive.drive_login');
+    }
+
+    public function post_drive_login_data(Request $request)
+    {
+        try{
+            if($request->username == "1" && $request->password =="1"){
+                return 'success';
+            }
+            else{
+                return 'wrong';
+            }
+        }catch (\Exception $exception){
+            return $exception;
+        }
+
+    }
+
     public function get_drive_page()
     {
         return view('pages.drive.drive');
