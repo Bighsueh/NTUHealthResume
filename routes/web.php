f06c58d71194cbf9e688110d1275185c3ede9318
yoyo <ya90312@gmail.com>
2022-01-04 02:10:12 +0800

    新增路由

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Facades\Route;
 //取得登入介面
 Route::get('/',[LoginController::class,'get_login_page'])->name('get_login_page');
 Route::post('/login',[LoginController::class,'post_login_data'])->name('post_login_data');
-//病患雲端系統
-Route::get('/drive', [DriveController::class,'get_drive_page'])->name('get_drive_page');
+//病患雲端系統登入介面
+Route::get('/drive/login', [DriveController::class,'get_drive_login_page'])->name('get_drive_login_page');
+Route::post('/drive/login', [DriveController::class,'post_drive_login_data'])->name('post_drive_login_data');
+Route::get('drive', [DriveController::class, 'get_drive_page'])->name('get_drive_page');
 
 Route::get('/setting/employee',[\App\Http\Controllers\EmployeeController::class,'get_setting_employee'])->name('get_setting_employee');
 
