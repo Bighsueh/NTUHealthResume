a9ee9014f4c04fdd5369c744226106210df59938
yoyo <ya90312@gmail.com>
2022-01-04 03:05:40 +0800

    病患雲端系統回饋單顯示呈現

--- resources/views/pages/drive/drive.blade.php
+++ resources/views/pages/drive/drive.blade.php
@@ -1,13 +1,30 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    123
-</body>
-</html>
+@extends('layouts.main.drive')
+
+@section('title')
+    病患雲端系統
+@endsection
+
+@section('content')
+    <div class="flex flex-column container w-full justify-center">
+        <h1 class="text-2xl font-mono">病患回饋單</h1>
+        <table class="table-auto border-collapse border border-gray-900">
+            <thead class="p-2 bg-gray-700 text-white text-xl">
+                <th>病患回饋單編號</th>
+                <th>標題</th>
+                <th>內容</th>
+            </thead>
+           <tr>
+               <td>1</td>
+               <td>title</td>
+               <td>content</td>
+           </tr>
+            <tr>
+                <td>2</td>
+                <td>title2</td>
+                <td>content2</td>
+            </tr>
+
+        </table>
+    </div>
+@endsection
+
