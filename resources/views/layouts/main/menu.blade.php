9de7a6ee794437ed2b62d5794bbe0fe3ec771c34
Bighsueh <40841121@gm.nfu.edu.tw>
2022-01-14 02:38:37 +0800

    註解掉 bootstrap

--- resources/views/layouts/main/menu.blade.php
+++ resources/views/layouts/main/menu.blade.php
@@ -9,14 +9,14 @@
 
     <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
     <script src="{{asset('assets/js/jquery-qrcode.min.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.js')}}"></script>
+{{--    <script src="{{asset('assets/js/bootstrap.js')}}"></script>--}}
     <script src="{{asset('assets/js/sweetalert2@11.js')}}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/font-awesome.js') }}"></script>
+{{--    <script type="text/javascript" src="{{ asset('assets/js/font-awesome.js') }}"></script>--}}
     <script type="text/javascript" src="{{ asset('assets/js/tailwind.min.js') }}"></script>
 
     <link href="{{ asset('assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
+{{--    <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css">--}}
+{{--    <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css">--}}
 
 </head>
 <body class="">
