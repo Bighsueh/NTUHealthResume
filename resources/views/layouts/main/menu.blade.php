fddb06b1692c8019f0277034ecaf6cd8e6558150
Bighsueh <40841121@gm.nfu.edu.tw>
2022-01-19 22:24:09 +0800

    加入tailwind cdn link

--- resources/views/layouts/main/menu.blade.php
+++ resources/views/layouts/main/menu.blade.php
@@ -12,12 +12,14 @@
 {{--    <script src="{{asset('assets/js/bootstrap.js')}}"></script>--}}
     <script src="{{asset('assets/js/sweetalert2@11.js')}}"></script>
 {{--    <script type="text/javascript" src="{{ asset('assets/js/font-awesome.js') }}"></script>--}}
-    <script type="text/javascript" src="{{ asset('assets/js/tailwind.min.js') }}"></script>
+{{--    <script type="text/javascript" src="{{ asset('assets/js/tailwind.min.js') }}"></script>--}}
 
-    <link href="{{ asset('assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css">
+{{--    <link href="{{ asset('assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css">--}}
 {{--    <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css">--}}
 {{--    <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css">--}}
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
 </head>
 <body class="">
 
