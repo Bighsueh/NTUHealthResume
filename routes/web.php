77db98656b6e1257a0603fae97cca9a29ecfcae7
yoyo <ya90312@gmail.com>
2022-02-22 23:28:46 +0800

    新增營養管理系統病患個別飲食紀錄頁面

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,9 @@ Route::get('/setting/patient',[\App\Http\Controllers\PatientController::class,'g
 
 Route::get('/setting/nutritionManagement',[\App\Http\Controllers\NutritionManagementController::class,'get_setting_nutritionManagement'])->name('get_setting_nutritionManagement');
 
-Route::get('/dietRecord',[\App\Http\Controllers\NutritionManagementController::class,'get_setting_nutritionManagement'])->name('dietRecord');
+Route::get('/dietRecord',function (){
+    return view('pages.setting.nutritionManagement.dietRecord');
+})->name('dietRecord');
 
 Route::get('/nutritionistComment',[\App\Http\Controllers\NutritionManagementController::class,'get_setting_nutritionManagement'])->name('nutritionistComment');
 
