5770629a12e383385a6ecd17eb038cd128f12189
Bighsueh <40841121@gm.nfu.edu.tw>
2022-02-24 21:44:03 +0800

    修正 MedicationRecordDetailTableSeeder 測試資料錯誤

--- database/seeders/MedicationRecordDetailTableSeeder.php
+++ database/seeders/MedicationRecordDetailTableSeeder.php
@@ -15,47 +15,47 @@ class MedicationRecordDetailTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('medication_records')->insert([
-            'detail_id' => '1',
-            'record_id' => '1',
+        DB::table('medication_record_detail')->insert([
+            'detail_id' => 1,
+            'record_id' => 1,
             'indication' => '有病治病無病強身',
             'category' => '仙丹',
             'trade_name' => '萬靈丹',
             'generic_name' => '靈芝',
-            'dose' => '3',
+            'dose' => 3,
             'routes' => '口服',
             'freq' => '一天三次',
-            'pres_day' => '3',
-            'total_amount' => '3',
+            'pres_day' => 3,
+            'total_amount' => 3,
             'note' => '無',
         ]);
-        DB::table('medication_records')->insert([
-            'detail_id' => '2',
-            'record_id' => '1',
+        DB::table('medication_record_detail')->insert([
+            'detail_id' => 2,
+            'record_id' => 1,
             'indication' => '有病治病無病強身',
             'category' => '仙丹',
             'trade_name' => '萬靈丹',
             'generic_name' => '靈芝',
-            'dose' => '3',
+            'dose' => 3,
             'routes' => '口服',
             'freq' => '一天三次',
-            'pres_day' => '3',
-            'total_amount' => '3',
+            'pres_day' => 3,
+            'total_amount' => 3,
             'note' => '無',
         ]);
-        DB::table('medication_records')->insert([
-            'detail_id' => '3',
-            'record_id' => '1',
-            'indication' => '有病治病無病強身',
-            'category' => '仙丹',
-            'trade_name' => '萬靈丹',
-            'generic_name' => '靈芝',
-            'dose' => '3',
-            'routes' => '口服',
-            'freq' => '一天三次',
-            'pres_day' => '3',
-            'total_amount' => '3',
-            'note' => '無',
-        ]);
+        DB::table('medication_record_detail')->insert([
+        'detail_id' => 3,
+        'record_id' => 1,
+        'indication' => '有病治病無病強身',
+        'category' => '仙丹',
+        'trade_name' => '萬靈丹',
+        'generic_name' => '靈芝',
+        'dose' => 3,
+        'routes' => '口服',
+        'freq' => '一天三次',
+        'pres_day' => 3,
+        'total_amount' => 3,
+        'note' => '無',
+    ]);
     }
 }
