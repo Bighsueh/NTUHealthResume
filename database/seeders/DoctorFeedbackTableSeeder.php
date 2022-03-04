c395e03f443bea2fc5cfc92f1f428353bf0e8593
Bighsueh <40841121@gm.nfu.edu.tw>
2022-03-04 22:16:47 +0800

    調整 DoctorFeedbackTableSeeder 欄位測資

--- database/seeders/DoctorFeedbackTableSeeder.php
+++ database/seeders/DoctorFeedbackTableSeeder.php
@@ -16,8 +16,7 @@ class DoctorFeedbackTableSeeder extends Seeder
     {
         DB::table('doctor_feedback')->insert([
             'doctor_feedback_id' => 1,
-            'patient_id' => 1,
-            'record_id' => 1,
+            'task_id' => 1,
             'doctor_id' => 1,
             'content' => '小弟，我看你骨骼精奇， 是百年難得一見的練武奇才呀！',
         ]);
