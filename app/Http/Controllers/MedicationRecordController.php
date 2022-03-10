a6eafd83182552167f7300521454a68f27bdee86
Bighsueh <40841121@gm.nfu.edu.tw>
2022-03-11 00:59:22 +0800

    加入 用藥管理系統-任務詳細資訊 儲存藥師回饋單,儲存醫師回饋單,帶入藥師回饋單資料,帶入醫師回饋單資料之後端function

--- app/Http/Controllers/MedicationRecordController.php
+++ app/Http/Controllers/MedicationRecordController.php
@@ -46,8 +46,21 @@ class MedicationRecordController extends Controller
             $task_id = $request->get('task_id');
 
             //藥歷列表
-            $medication_records = DB::table('medication_records')->get();
+            $medication_records =
+                DB::table('medication_records')
+                    ->get();
 
+            //藥師回饋單
+            $pharmacist_feedback =
+                DB::table('pharmacist_feedback')
+                    ->where('task_id', $task_id)
+                    ->first();
+
+            //醫師回饋單
+            $doctor_feedback =
+                DB::table('doctor_feedback')
+                ->where('task_id',$task_id)
+                ->first();
 
             foreach ($medication_records as $row) {
                 $record_id = $row->record_id;
@@ -58,7 +71,12 @@ class MedicationRecordController extends Controller
                         ->get();
             }
 
-            $result = ['task_id' => $task_id, 'medication_records' => $medication_records];
+            $result = [
+                'task_id' => $task_id,
+                'medication_records' => $medication_records,
+                'pharmacist_feedback' => $pharmacist_feedback,
+                'doctor_feedback' => $doctor_feedback,
+            ];
             return view('pages.medicationManagement.taskDetail', $result);
         } catch (\Exception $exception) {
             return $exception;
@@ -66,260 +84,97 @@ class MedicationRecordController extends Controller
 
     }
 
-    //開啟用藥管理系統-個別病患紀錄頁面
-    public function get_medication_record_and_feedback_management_patient_detail_page(Request $request)
-    {
-//        $doctor_feedback = DB::table('doctor_feedback')->where('record_id', 1)->first();
-//        dd($doctor_feedback->content);
-
-        $patient_id = $request->get('patient_id');
-        $patient_data = DB::table('patients')->where('patient_id', $patient_id)->first();
-        $medication_record = DB::table('medication_records')->get();
-        $user_name = Session::get('user_name');
-
-        $result = [
-            'medication_record' => $medication_record,
-            'patient_name' => $patient_data->patient_name,
-            'patient_id' => $patient_data->patient_id,
-            'user_name' => $user_name,
-        ];
-
-        return view('pages.medicationManagement.patientDetail', $result);
-    }
-
-    //依record_id取得藥物子項目
-    public function get_medication_record_detail(Request $request)
-    {
-        //record_id
-        $record_id = $request->record_id;
-
-        $main_record = DB::table('medication_records')->where('record_id', $record_id)->first();
-        $record_list = DB::table('medication_record_detail')->where('record_id', $record_id)->get();
-
-        $result = ['main_record' => $main_record, 'record_list' => $record_list];
-        return $result;
-    }
-
-    //取得醫師回覆內容
-    public function get_doctor_feedback(Request $request)
+    //儲存藥師回饋單內容
+    public function store_pharmacist_feedback_data(Request $request)
     {
         try {
-            //藥歷id
-            $record_id = $request->record_id;
-            //查詢醫師回饋單
-            $doctor_feedback = DB::table('doctor_feedback')->where('record_id', $record_id)->first();
+            //要回覆病患的內容
+            $pharmacist_reply = $request->get('pharmacist_reply');
+            //向醫師提問(選填)
+            $ask_to_doctor = $request->get('ask_to_doctor');
+            //task_id
+            $task_id = $request->get('task_id');
+            //user_name
+            $user_name = $request->session()->get('user_name');
 
-            //若存在醫師回饋單則取出回覆內容
-            if ($doctor_feedback) {
-                return $doctor_feedback->content;
+            //先確認是否存在藥師回覆紀錄
+            $reply_check =
+                DB::table('pharmacist_feedback')
+                    ->where('task_id', $task_id)
+                    ->first();
+
+            //若存在藥師回覆紀錄
+            if ($reply_check) {
+                DB::table('pharmacist_feedback')
+                    ->where('task_id', $task_id)
+                    ->update([
+                        'task_id' => $task_id,
+                        'pharmacist_id' => $user_name,
+                        'pharmacist_reply' => $pharmacist_reply,
+                        'pharmacist_question' => $ask_to_doctor,
+                        'updated_at' => Carbon::now(),
+                    ]);
             }
 
-            //若不存在醫師回饋單則回傳空字串
-            if ($doctor_feedback) {
-                return "";
+            //若不存在藥師回覆紀錄
+            if (!$reply_check) {
+                DB::table('pharmacist_feedback')
+                    ->insert([
+                        'task_id' => $task_id,
+                        'pharmacist_id' => $user_name,
+                        'pharmacist_reply' => $pharmacist_reply,
+                        'pharmacist_question' => $ask_to_doctor,
+                        'created_at' => Carbon::now(),
+                    ]);
             }
 
-        } catch (Exception $exception) {
+            return 'success';
+        } catch (\Exception $exception) {
             return $exception;
         }
     }
 
-    //儲存醫師回饋單,醫師回饋及醫師發問內容
-    public function store_doctor_feedback(Request $request)
+    //儲存醫師回饋單內容
+    public function store_doctor_feedback_data(Request $request)
     {
         try {
-            //藥歷id
-            $record_id = $request->record_id;
-            //醫師回覆內容
-            $doctor_reply = $request->doctor_reply;
-            //提師提問內容
-            $doctor_ask = $request->doctor_ask;
+            //醫師回覆藥師的內容
+            $doctor_reply = $request->get('doctor_reply');
+            //task_id
+            $task_id = $request->get('task_id');
 
-            //先取得病患的資料
-            $patient_data = DB::table('medication_records')->where('record_id', $record_id)->first();
-
-            //確認醫師回饋資料是否存在
-            $if_exist_doctor_feedback = DB::table('doctor_feedback')->where('record_id', $record_id)->count();
-            //確認藥師資料是否存在
-            $if_exist_pharmacist_feedback = DB::table('pharmacist_feedback')->where('record_id', $record_id)->count();
-
-            //若以存在資料則導向修改資料
-            if ($if_exist_doctor_feedback > 0) {
-                //修改資料
+            $reply_check =
                 DB::table('doctor_feedback')
-                    ->where('record_id', $record_id)
+                    ->where('task_id', $task_id)
+                    ->first();
+
+            //若存在醫師回覆紀錄
+            if ($reply_check) {
+                DB::table('doctor_feedback')
+                    ->where('task_id', $task_id)
                     ->update([
-                        'patient_id' => $patient_data->patient_id,
-                        'record_id' => $record_id,
-                        'doctor_id' => 1,
-                        'content' => $doctor_reply,
+                        'task_id' => $task_id,
+                        'doctor_id' => $request->session()->get('user_name'),
+                        'doctor_reply' => $doctor_reply,
                         'updated_at' => Carbon::now(),
                     ]);
             }
-//
-            //若不存在資料則導向則新增資料
-            if ($if_exist_doctor_feedback === 0) {
-                //新增資料
-                DB::table('doctor_feedback')->insert([
-                    'patient_id' => $patient_data->patient_id,
-                    'record_id' => $record_id,
-                    'doctor_id' => 1,
-                    'content' => $doctor_reply,
-                    'created_at' => Carbon::now(),
-                ]);
-            }
 
-            //若以存在資料則導向修改資料
-            if ($if_exist_pharmacist_feedback > 0) {
-                //修改資料
-                DB::table('pharmacist_feedback')
-                    ->where('record_id', $record_id)
-                    ->update([
-                        'patient_id' => $patient_data->patient_id,
-                        'record_id' => $record_id,
-                        'pharmacist_id' => 1,
-                        'doctor_ask' => $doctor_ask,
-                        'updated_at' => Carbon::now(),
+            //若不存在醫師回覆紀錄
+            if (!$reply_check) {
+                DB::table('doctor_feedback')
+                    ->insert([
+                        'task_id' => $task_id,
+                        'doctor_id' => $request->session()->get('user_name'),
+                        'doctor_reply' => $doctor_reply,
+                        'created_at' => Carbon::now(),
                     ]);
             }
-//
-            //若不存在資料則導向則新增資料
-            if ($if_exist_pharmacist_feedback === 0) {
-                //新增資料
-                DB::table('pharmacist_feedback')->insert([
-                    'patient_id' => $patient_data->patient_id,
-                    'record_id' => $record_id,
-                    'pharmacist_id' => 1,
-                    'doctor_ask' => $doctor_ask,
-                    'created_at' => Carbon::now(),
-                ]);
-            }
 
-
-            return 'success';
-        } catch (Exception $exception) {
-            return $exception;
-        }
-    }
-
-    //取得藥師回覆內容
-    public function get_pharmacist_feedback(Request $request)
-    {
-        try {
-            //藥歷id
-            $record_id = $request->record_id;
-
-            //查詢藥師回饋單
-            $pharmacist_feedback = DB::table('pharmacist_feedback')->where('record_id', $record_id)->first();
-
-            //若存在藥師回饋單則取出回覆內容
-            if ($pharmacist_feedback) {
-                return $pharmacist_feedback;
-            }
-
-            //若不存在藥師回饋單則回傳空字串
-            if ($pharmacist_feedback) {
-                return "";
-            }
-        } catch (\Exception $exception) {
-            return $exception;
-        }
-    }
-
-    //儲存藥師回覆內容
-    public function store_pharmacist_feedback(Request $request)
-    {
-        try {
-            //藥歷id
-            $record_id = $request->record_id;
-            //要師回覆
-            $pharmacist_reply = $request->pharmacist_reply;
-
-            //先取得病患的資料
-            $patient_data = DB::table('medication_records')->where('record_id', $record_id)->first();
-
-            //先確認資料是否存在
-            $if_exist = DB::table('pharmacist_feedback')->where('record_id', $record_id)->count();
-
-            //若以存在資料則導向修改資料
-            if ($if_exist > 0) {
-                //修改資料
-                DB::table('pharmacist_feedback')
-                    ->where('record_id', $record_id)
-                    ->update([
-                        'patient_id' => $patient_data->patient_id,
-                        'record_id' => $record_id,
-                        'pharmacist_id' => 1,
-                        'content' => $pharmacist_reply,
-                        'updated_at' => Carbon::now(),
-                    ]);
-            }
-//
-            //若不存在資料則導向則新增資料
-            if ($if_exist === 0) {
-                //新增資料
-                DB::table('pharmacist_feedback')->insert([
-                    'patient_id' => $patient_data->patient_id,
-                    'record_id' => $record_id,
-                    'pharmacist_id' => 1,
-                    'content' => $pharmacist_reply,
-                    'created_at' => Carbon::now(),
-                ]);
-            }
-
-            return "success";
-        } catch (\Exception $exception) {
-            return $exception;
-        }
-    }
-
-//    儲存藥物明細資訊
-    public function store_medication_record_detail(Request $request)
-    {
-        try {
-            $date_of_examination = $request->date_of_examination;
-            $redate = $request->redate;
-            $pres_hosp = $request->pres_hosp;
-            $disp_hosp = $request->disp_hosp;
-            $patient_id = $request->patient_id;
-
-            DB::table('medication_records')
-                ->insert([
-                    'date_of_examination' => $date_of_examination,
-                    'redate' => $redate,
-                    'pres_hosp' => $pres_hosp,
-                    'disp_hosp' => $disp_hosp,
-                    'patient_id' => $patient_id,
-                    'created_at' => Carbon::now(),
-                ]);
             return 'success';
         } catch (\Exception $exception) {
             return $exception;
         }
     }
 
-    //儲存藥物明細修改資料
-    public function update_medication_record_detail(Request $request)
-    {
-        try {
-            $date_of_examination = $request->date_of_examination;
-            $redate = $request->redate;
-            $pres_hosp = $request->pres_hosp;
-            $disp_hosp = $request->disp_hosp;
-            $record_id = $request->record_id;
-
-            DB::table('medication_records')
-                ->where('record_id', $record_id)
-                ->update([
-                    'date_of_examination' => $date_of_examination,
-                    'redate' => $redate,
-                    'pres_hosp' => $pres_hosp,
-                    'disp_hosp' => $disp_hosp,
-                    'updated_at' => Carbon::now(),
-                ]);
-            return 'success';
-        } catch (\Exception $exception) {
-            return $exception;
-        }
-    }
 }
