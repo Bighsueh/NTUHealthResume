596820fcb585c6934ce5ee4713f3ba8fbe5bf103
Bighsueh <40841121@gm.nfu.edu.tw>
2022-03-11 00:53:01 +0800

    加入 用藥管理系統-任務詳細資料頁面 藥師回饋modal

--- resources/views/pages/medicationManagement/modal/pharmacistFeedbackModal.blade.php
+++ resources/views/pages/medicationManagement/modal/pharmacistFeedbackModal.blade.php
@@ -13,10 +13,10 @@
                         data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body relative p-4">
-                <!--醫師向藥師提問-->
+                <!--藥師回覆病患-->
                 <div class="flex justify-center">
                     <div class="mb-3 xl:w-full">
-                        <b class="mx-2">醫師提問內容</b>
+                        <b class="mx-2">要回覆病患的內容</b>
                         <textarea
                             class="
                             form-control
@@ -34,52 +34,54 @@
                             ease-in-out
                             m-0
                             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            id="pharmacist_feedback_modal_doctor_reply"
-                            rows="5"
-                            placeholder="醫師尚未填寫內容"
-                            disabled
-                        ></textarea>
-                    </div>
-                </div>
-                <!--醫師向藥師提問-->
-                <div class="flex justify-center">
-                    <div class="mb-3 xl:w-full">
-                        <b class="mx-2">藥師回覆</b>
-                        <textarea
-                            class="
-                            form-control
-                            block
-                            w-full
-                            px-3
-                            py-1.5
-                            text-base
-                            font-normal
-                            text-gray-700
-                            bg-white bg-clip-padding
-                            border border-solid border-gray-300
-                            rounded
-                            transition
-                            ease-in-out
-                            m-0
-                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            id="pharmacist_feedback_modal_reply_text"
+                            id="pharmacist_feedback_modal_pharmacist_reply"
                             rows="5"
                             placeholder="請在此處填寫內容...  (Shift + Enter可以換行)"
-                        ></textarea>
+{{--                            disabled--}}
+                        >{{$pharmacist_feedback->pharmacist_reply}}</textarea>
+                    </div>
+                </div>
+                <!--藥師向醫師提問-->
+                <div class="flex justify-center">
+                    <div class="mb-3 xl:w-full">
+                        <b class="mx-2">向醫師提問(選填)</b>
+                        <textarea
+                            class="
+                            form-control
+                            block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="pharmacist_feedback_modal_ask_to_doctor"
+                            rows="5"
+                            placeholder="請在此處填寫內容...  (Shift + Enter可以換行)"
+                        >{{$pharmacist_feedback->pharmacist_question}}</textarea>
                     </div>
                 </div>
             </div>
             <div
                 class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
                 <button type="button"
-                        class="btn-pharmacist-feedback-save mx-2 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded"
-                        data-bs-dismiss="modal">
-                    儲存並關閉
-                </button>
-                <button type="button"
-                        class="btn-pharmacist-feedback-cancel mx-2 bg-transparent border border-red-700 text-red-700 hover:bg-red-700 hover:text-white text-center py-2 px-4 rounded"
+                        id = "btn-pharmacist-feedback-cancel"
+                        class="mx-2 bg-transparent border border-red-700 text-red-700 hover:bg-red-700 hover:text-white text-center py-2 px-4 rounded"
                         data-bs-dismiss="modal">
                     不儲存並關閉
+                </button>
+                <button type="button"
+                        id = "btn-pharmacist-feedback-save"
+                        class="mx-2 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded"
+                        data-bs-dismiss="modal">
+                    儲存並關閉
                 </button>
                 {{--                <button type="button"--}}
                 {{--                        class="mx-2 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">--}}
@@ -91,59 +93,52 @@
 </div>
 <script>
     $(function () {
-        let record_id;
-        //儲存藥師回覆
-        $(".btn-pharmacist-feedback-save").click(function () {
-            let url = "store_pharmacist_feedback";
-            let pharmacist_reply = $("#pharmacist_feedback_modal_reply_text").val();
 
-            $.ajax({
-                url: url,
-                method: "post",
-                data: {
-                    "_token": "{{csrf_token()}}",
-                    "record_id": record_id,
-                    "pharmacist_reply": pharmacist_reply
-                },
-                success: function (res) {
-                    console.log(res);
+    //儲存藥師回覆給病患的內容
+    $('#btn-pharmacist-feedback-save').click(function(){
+        //要回覆病患的內容
+        let pharmacist_reply = $('#pharmacist_feedback_modal_pharmacist_reply').val();
+        //向醫師提問(選填)
+        let ask_to_doctor = $('#pharmacist_feedback_modal_ask_to_doctor').val();
+
+        let url = "{{route('store_medication_management_pharmacist_feedback_data')}}";
+
+        $.ajax({
+            url: url,
+            method: "post",
+            data: {
+                "_token": "{{csrf_token()}}",
+                "task_id" : $('#task_id').text(),
+                "pharmacist_reply": pharmacist_reply,
+                "ask_to_doctor": ask_to_doctor
+            },
+            success: function (res) {
+                if (res === 'success') {
+                    Swal.fire({
+                        icon: 'success',
+                        title: '藥師回覆儲存成功',
+                        confirmButtonColor: '#8CD4F5'
+                    })
                 }
-            })
+
+            },
+            error : function(res){
+                //錯誤訊息
+                console.log(res);
+
+                Swal.fire({
+                    icon: 'error',
+                    title: '儲存失敗',
+                    text: res['statusText'],
+                    confirmButtonColor: '#8CD4F5'
+                })
+            }
         })
 
-        //開啟藥師回饋單modal
-        $(".btn-pharmacist-feedback").click(function () {
-            //取得藥物id
-            record_id = $(this).parent().parent().find('.medication_record_id').text();
 
-            //清空藥師回覆欄位
-            $("#pharmacist_feedback_modal_reply_text").val('');
-
-            //清空醫師回覆欄位
-            $("#pharmacist_feedback_modal_doctor_reply").val("");
-
-            update_textarea();
-        });
-
-        //更新藥師回覆及醫師提問欄位
-        function update_textarea(){
-            let url = "{{route('get_pharmacist_feedback')}}";
-
-            $.ajax({
-                url:url,
-                method:'post',
-                data:{
-                    "_token":"{{csrf_token()}}",
-                    "record_id":record_id,
-                },
-                success:function(res){
-                    $("#pharmacist_feedback_modal_doctor_reply").val(res['doctor_ask']);
-                    $("#pharmacist_feedback_modal_reply_text").val(res['content']);
+    })
 
 
-                }
-            })
-        }
     })
 
 </script>
