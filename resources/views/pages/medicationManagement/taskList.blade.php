7cedacf7a55a06559a00ac3467ee2654568d1415
Bighsueh <40841121@gm.nfu.edu.tw>
2022-03-11 00:56:10 +0800

    調整 用藥管理系統-任務列表頁面 排版

--- resources/views/pages/medicationManagement/taskList.blade.php
+++ resources/views/pages/medicationManagement/taskList.blade.php
@@ -59,88 +59,88 @@
         </div>
         <!--content status & search bar w container-->
 
-    <div class="lg:p-8 md:p-6 sm:p-2 max-h-screen h-screen  bg-gray-200">
-        @include('pages.menu.statusBar')
+        <div class="lg:p-8 md:p-6 sm:p-2 max-h-screen h-screen  bg-gray-200">
+            @include('pages.menu.statusBar')
 
-        <div class="grid">
-            <div class="flex justify-between ">
-                <p class="mx-4 my-2 justify-self-start font-bold text-xl">任務列表</p>
-{{--                <div class="flex mx-4">--}}
-{{--                    <a href=""--}}
-{{--                       class="bg-teal-700 mx-2 justify-self-end border border-teal-700 hover:border-teal-500 text-gray-50 hover:bg-teal-500 hover:text-white text-center py-2 px-4 rounded">--}}
-{{--                        新增病患回饋單--}}
-{{--                    </a>--}}
-{{--                    <a href=""--}}
-{{--                       class="bg-teal-700 mx-2 justify-self-end border border-teal-700 hover:border-teal-500 text-gray-50 hover:bg-teal-500 hover:text-white text-center py-2 px-4 rounded">--}}
-{{--                        新增藥師回饋單--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-            </div>
-            <div class="flex">
-                <!--數據欄位-->
-                <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-                    <table class="divide-y divide-gray-200 min-w-full">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                #
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                任務狀態
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                任務建立日期
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                任務結案日期
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                最後異動時間
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                任務詳情
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="divide-y divide-gray-200">
-                        <a hidden>{{$thread = 0}}</a>
-                        @foreach($task_list as $row)
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{$thread +=1}}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{isset($row->status) ? $row->status : '-'}}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{isset($row->created_at) ? $row->created_at : '-'}}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{isset($row->finish_date) ? $row->finish_date : '-'}}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{isset($row->updated_at) ? $row->updated_at : '-'}}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <a href="{{route('get_medication_management_task_detail_page')}}"
-                                   class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
-                                    查看
-                                </a>
-                            </td>
+            <div class="grid">
+                <div class="flex justify-between ">
+                    <p class="mx-4 my-2 justify-self-start font-bold text-xl">任務列表</p>
+                    {{--                <div class="flex mx-4">--}}
+                    {{--                    <a href=""--}}
+                    {{--                       class="bg-teal-700 mx-2 justify-self-end border border-teal-700 hover:border-teal-500 text-gray-50 hover:bg-teal-500 hover:text-white text-center py-2 px-4 rounded">--}}
+                    {{--                        新增病患回饋單--}}
+                    {{--                    </a>--}}
+                    {{--                    <a href=""--}}
+                    {{--                       class="bg-teal-700 mx-2 justify-self-end border border-teal-700 hover:border-teal-500 text-gray-50 hover:bg-teal-500 hover:text-white text-center py-2 px-4 rounded">--}}
+                    {{--                        新增藥師回饋單--}}
+                    {{--                    </a>--}}
+                    {{--                </div>--}}
+                </div>
+                <div class="flex">
+                    <!--數據欄位-->
+                    <div class="rounded m-2 flex-1 bg-gray-50 p-4">
+                        <table class="divide-y divide-gray-200 min-w-full">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    #
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    任務狀態
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    任務建立日期
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    任務結案日期
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    最後異動時間
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
+                                    任務詳情
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200">
+                            <a hidden>{{$thread = 0}}</a>
+                            @foreach($task_list as $row)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{$thread +=1}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{isset($row->status) ? $row->status : '-'}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{isset($row->created_at) ? $row->created_at : '-'}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{isset($row->finish_date) ? $row->finish_date : '-'}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{isset($row->updated_at) ? $row->updated_at : '-'}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <a href="{{route('get_medication_management_task_detail_page',['task_id'=>$row->task_id])}}"
+                                           class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
+                                            查看
+                                        </a>
+                                    </td>
 
-                        </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
