591be644668fdb6eb077a6f6c27b52a56a64fff2
Bighsueh <40841121@gm.nfu.edu.tw>
2022-03-16 21:30:19 +0800

    調整 用用藥管理系統-任務詳細資訊頁面 功能操作列表

--- resources/views/pages/medicationManagement/taskDetail.blade.php
+++ resources/views/pages/medicationManagement/taskDetail.blade.php
@@ -35,6 +35,10 @@
                     <div class="flex">
                         <div class="mx-6 mt-4 flex items-end">
                             <p class="text-3xl mb-2 font-bold">用藥管理系統</p>
+                            <p class="md:block hidden">md　</p>
+                            <p class="lg:block hidden">lg　</p>
+                            <p class="xl:block hidden">xl　</p>
+                            <p class="2xl:block hidden">2xl　</p>
                             {{--                        <div class="flex item-end mx-4 text-gray-400">--}}
                             {{--                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-4" fill="none"--}}
                             {{--                                 viewBox="0 0 24 24" stroke="currentColor">--}}
@@ -65,34 +69,31 @@
             <!--數據統計-->
             <div class="grid">
                 <p class="mx-4 my-2 justify-self-start font-bold text-xl">功能操作</p>
-                <div class="flex">
+                <div class="grid lg:grid-cols-2 2xl:grid-cols-4">
                     <!--數據欄位-->
                     <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-                        <div class="flex items-center mx-4 px-5 my-2">
+{{--                        <div class="flex items-center md:mx-4 md:px-5 my-2">--}}
+                        <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                             <div class="flex grid">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600"
                                      fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4"/>
                                 </svg>
                             </div>
-                            <div class="mx-4 px-5">
-                                <p class="mb-4 text-xl text-gray-600">藥師回饋</p>
+                            <div class="mx-2 px-5 grid grid-cols-1 ">
+                                <p class="col-span-1 text-xl text-gray-600">新增藥歷</p>
                                 <a
-                                   id="btn_pharmacist_feedback"
-                                   data-bs-toggle="modal" data-bs-target="#pharmacistModal"
-                                   class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
+                                    id="btn-create-medication-record"
+                                    class="col-span-1 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 x-4 rounded">
                                     查看
                                 </a>
                             </div>
                         </div>
-                        {{--                        <div class="mx-4 my-2 px-5">--}}
-                        {{--                            <a href="" class="mt-2 text-green-700">查看資訊</a>--}}
-                        {{--                        </div>--}}
                     </div>
                     <!--數據欄位-->
                     <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-                        <div class="flex items-center mx-4 px-5 my-2">
+                        {{--                        <div class="flex items-center md:mx-4 md:px-5 my-2">--}}
+                        <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                             <div class="flex grid">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600"
                                      fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -100,43 +101,41 @@
                                           d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"/>
                                 </svg>
                             </div>
-                            <div class="mx-4 px-5">
-                                <p class="mb-4 text-xl text-gray-600">其他資訊</p>
+                            <div class="mx-2 px-5 grid grid-cols-1 ">
+                                <p class="col-span-1 text-xl text-gray-600">其他資訊</p>
                                 <a
-                                   id="btn_other_information"
-                                   data-bs-toggle="modal" data-bs-target="#OtherInformationModal"
-                                   class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
+                                    id="btn_other_information"
+                                    class="col-span-1 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 x-4 rounded">
                                     查看
                                 </a>
                             </div>
                         </div>
-                        {{--                        <div class="mx-4 my-2 px-5">--}}
-                        {{--                            <a href="" class="mt-2 text-green-700">查看資訊</a>--}}
-                        {{--                        </div>--}}
                     </div>
                     <!--數據欄位-->
                     <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-                        <div class="flex items-center mx-4 px-5 my-2">
+                        {{--                        <div class="flex items-center md:mx-4 md:px-5 my-2">--}}
+                        <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                             <div class="flex grid">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600"
                                      fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"/>
                                 </svg>
                             </div>
-                            <div class="mx-4 px-5">
-                                <p class="mb-4 text-xl text-gray-600">新增藥歷</p>
+                            <div class="mx-2 px-5 grid grid-cols-1 ">
+                                <p class="col-span-1 text-xl text-gray-600">藥師回饋</p>
                                 <a
-                                   id="btn-create-medication-record"
-                                   class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
+                                    id="btn_pharmacist_feedback"
+                                    class="col-span-1 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 x-4 rounded">
                                     查看
                                 </a>
                             </div>
                         </div>
-
                     </div>
                     <!--數據欄位-->
                     <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-                        <div class="flex items-center mx-4 px-5 my-2">
+                        {{--                        <div class="flex items-center md:mx-4 md:px-5 my-2">--}}
+                        <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                             <div class="flex grid">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600"
                                      fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -144,16 +143,15 @@
                                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                 </svg>
                             </div>
-                            <div class="mx-4 px-5">
-                                <p class="mb-4 text-xl text-gray-600">案件狀態</p>
+                            <div class="mx-2 px-5 grid grid-cols-1 ">
+                                <p class="col-span-1 text-xl text-gray-600">案件狀態</p>
                                 <a
-                                   id="btn-progress-status"
-                                   class="bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 px-4 rounded">
+                                    id="btn-progress-status"
+                                    class="col-span-1 bg-transparent border border-teal-700 text-teal-700 hover:bg-teal-700 hover:text-white text-center py-2 x-4 rounded">
                                     查看
                                 </a>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -167,7 +165,7 @@
                     @foreach($medication_records as $row)
                         <div class="flex ">
                             <!--單筆藥歷共通項目-->
-                            <div class="rounded m-2 flex-auto bg-gray-50 p-4 w-1/6">
+                            <div class="rounded m-2 flex-none bg-gray-50 p-4 w-1/6">
                                 <p>就醫日期：</p>
                                 <li>{{$row->date_of_examination}}</li>
                                 <p>開方日期：</p>
@@ -186,10 +184,10 @@
                                             class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
                                             適應症
                                         </th>
-                                        <th scope="col"
-                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                            藥物種類
-                                        </th>
+{{--                                        <th scope="col"--}}
+{{--                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">--}}
+{{--                                            藥物種類--}}
+{{--                                        </th>--}}
                                         <th scope="col"
                                             class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
                                             藥品名稱
@@ -210,34 +208,34 @@
                                             class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
                                             頻率
                                         </th>
-                                        <th scope="col"
-                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                            處方天數
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                            總數
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">
-                                            相關註記
-                                        </th>
+{{--                                        <th scope="col"--}}
+{{--                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">--}}
+{{--                                            處方天數--}}
+{{--                                        </th>--}}
+{{--                                        <th scope="col"--}}
+{{--                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">--}}
+{{--                                            總數--}}
+{{--                                        </th>--}}
+{{--                                        <th scope="col"--}}
+{{--                                            class="px-6 py-1 text-left font-medium text-gray-500 text-nowrap whitespace-nowrap tracking-wider">--}}
+{{--                                            相關註記--}}
+{{--                                        </th>--}}
 
                                     </tr>
                                     </thead>
                                     <tbody class="divide-y divide-gray-200">
                                     @foreach($row->record_detail as $detail_row)
                                         <tr>
-                                            <td>{{$detail_row->indication}}</td>
-                                            <td>{{$detail_row->category}}</td>
-                                            <td>{{$detail_row->trade_name}}</td>
-                                            <td>{{$detail_row->generic_name}}</td>
-                                            <td>{{$detail_row->dose}}</td>
-                                            <td>{{$detail_row->routes}}</td>
-                                            <td>{{$detail_row->freq}}</td>
-                                            <td>{{$detail_row->pres_day}}</td>
-                                            <td>{{$detail_row->total_amount}}</td>
-                                            <td>{{$detail_row->total_amount}}</td>
+                                            <td class="text-left px-6">{{$detail_row->indication}}</td>
+{{--                                            <td class=text-left px-6">{{$detail_row->category}}</td>--}}
+                                            <td class="text-left px-6">{{$detail_row->trade_name}}</td>
+                                            <td class="text-left px-6">{{$detail_row->generic_name}}</td>
+                                            <td class="text-left px-6">{{$detail_row->dose}}</td>
+                                            <td class="text-left px-6">{{$detail_row->routes}}</td>
+                                            <td class="text-left px-6">{{$detail_row->freq}}</td>
+{{--                                            <td>{{$detail_row->pres_day}}</td>--}}
+{{--                                            <td>{{$detail_row->total_amount}}</td>--}}
+{{--                                            <td>{{$detail_row->total_amount}}</td>--}}
                                         </tr>
                                     @endforeach
                                     </tbody>
