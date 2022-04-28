2c91448ad077f20270299d7d3c577cdf7c754273
“{gayZ3A20}” <{gay195587@gmail.com}>
2022-04-28 21:44:35 +0800

    完成 MedicationRecordDetailImport

---
+++ app/Imports/MedicationRecordDetailImport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use App\Models\MedicationRecordDetail;
+use Illuminate\Support\Facades\Log;
+
+class MedicationRecordDetailImport implements ToCollection
+{
+
+    public function collection(Collection $rows)
+    {
+        $count = 0;
+       Log::debug($rows);
+        foreach ($rows as $row)
+        {
+
+
+//            Log::debug($row[5]);
+            if($count == 0) {
+                $count = $count + 1;
+                continue;
+            }
+
+            if($row[5]==null){
+                continue;
+            }
+
+            Log::debug($row[5]);
+            MedicationRecordDetail::create([
+                'record_id' => $row[0],
+                'trade_name' => $row[5],
+                'generic_name' => $row[6],
+                'dose' => $row[7],
+                'freq' => $row[8]
+            ]);
+        }
+    }
+}
