e771d8d633d2674810c26412cb4da234f4b8ef80
Bighsueh <40841121@gm.nfu.edu.tw>
2022-05-14 00:02:31 +0800

    rename other_infomation migration

--- database/migrations/2022_05_13_151655_create_other_infomation_table.php
+++ database/migrations/2022_05_13_151655_create_other_infomation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDoctorFeedbackTable extends Migration
+class CreateOtherInfomationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateDoctorFeedbackTable extends Migration
      */
     public function up()
     {
-        Schema::create('doctor_feedback', function (Blueprint $table) {
-            $table->id('doctor_feedback_id')->autoIncrement();
+        Schema::create('other_information', function (Blueprint $table) {
+            $table->id('other_info_id')->autoIncrement();
             $table->integer('task_id');
             $table->integer('doctor_id');
-            $table->text('doctor_reply')->nullable();
+            $table->text('doctor_comment')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateDoctorFeedbackTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('doctor_feedback');
+        Schema::dropIfExists('other_infomation');
     }
 }
