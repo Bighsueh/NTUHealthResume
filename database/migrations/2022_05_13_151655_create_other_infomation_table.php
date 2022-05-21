62a4628a227aea5905873a2dcd2dd2e08f04bf44
Bighsueh <40841121@gm.nfu.edu.tw>
2022-05-21 22:54:37 +0800

    加入 other_information migration

--- database/migrations/2022_05_13_151655_create_other_infomation_table.php
+++ database/migrations/2022_05_13_151655_create_other_infomation_table.php
@@ -16,8 +16,29 @@ class CreateOtherInfomationTable extends Migration
         Schema::create('other_information', function (Blueprint $table) {
             $table->id('other_info_id')->autoIncrement();
             $table->integer('task_id');
-            $table->integer('doctor_id');
-            $table->text('doctor_comment')->nullable();
+            $table->string('Weight')->nullable();
+            $table->string('Height')->nullable();
+            $table->string('Diagnosis')->nullable();
+            $table->string('Surgery')->nullable();
+            $table->string('Hb')->nullable();
+            $table->string('Alb')->nullable();
+            $table->string('AST')->nullable();
+            $table->string('Cre')->nullable();
+            $table->string('CCr')->nullable();
+            $table->string('Suger')->nullable();
+            $table->string('HbA1C')->nullable();
+            $table->string('TCHO')->nullable();
+            $table->string('TG')->nullable();
+            $table->string('LDL')->nullable();
+            $table->string('UA')->nullable();
+            $table->string('HDL')->nullable();
+            $table->string('Fall')->nullable();
+            $table->string('medication_adherence_1')->nullable();
+            $table->string('medication_adherence_2')->nullable();
+            $table->string('medication_adherence_3')->nullable();
+            $table->string('drug_side_effect')->nullable();
+            $table->string('drug_side_effect_text')->nullable();
+            $table->text('other_information_modal_textarea')->nullable();
             $table->timestamps();
         });
     }
