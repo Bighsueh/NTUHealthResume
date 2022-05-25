215763af58d0bca381652b7c6f9f5d032791ff50
Bighsueh <40841121@gm.nfu.edu.tw>
2022-05-25 20:58:37 +0800

    更新 員工管理-編輯員工列表欄位 將"結案日期"改成"收案日期"

--- resources/views/pages/setting/patient/settingPatientModal.blade.php
+++ resources/views/pages/setting/patient/settingPatientModal.blade.php
@@ -37,7 +37,7 @@
                                 <div class="relative z-0 mb-4 w-full group">
                                     <input id="edit_close_date"
                                            type="email" name="floating_email" class="block py-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-teal-600 peer" placeholder=" " required />
-                                    <label for="floating_email" class="absolute text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-teal-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">結案日期</label>
+                                    <label for="floating_email" class="absolute text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-teal-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">收案日期</label>
                                 </div>
                                 <div class="relative z-0 mb-4 w-full group">
                                     <input id="edit_place"
