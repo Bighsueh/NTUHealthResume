15782d1bbe62baad311797f859d9161b1a6c36f1
Bighsueh <40841121@gm.nfu.edu.tw>
2022-05-26 23:27:08 +0800

    調整 主頁面-數據統計列 欄位介面

--- resources/views/pages/menu/statusBar.blade.php
+++ resources/views/pages/menu/statusBar.blade.php
@@ -4,63 +4,46 @@
     <div class="flex">
         <!--數據欄位-->
         <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-            <div class="flex items-center mx-4 px-5 my-2">
+            <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                 <div class="flex grid">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         class="h-8 w-8 justify-self-end text-gray-600" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                 </div>
                 <div class="mx-4 px-5">
-                    <p class="text-xl text-gray-600">標題</p>
-                    <p class="text-2xl text-gray-900">數據</p>
+                    <p class="text-xl text-gray-600">病患人數統計</p>
+                    <p class="text-2xl text-gray-900" id="task_nums">? 件</p>
                 </div>
-            </div>
-            <div class="mx-4 my-2 px-5">
-                <a href="" class="mt-2 text-green-700">查看資訊</a>
             </div>
         </div>
         <!--數據欄位-->
         <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-            <div class="flex items-center mx-4 px-5 my-2">
+            <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                 <div class="flex grid">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         class="h-8 w-8 justify-self-end text-gray-600" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"/>
                     </svg>
                 </div>
                 <div class="mx-4 px-5">
-                    <p class="text-xl text-gray-600">標題</p>
-                    <p class="text-2xl text-gray-900">數據</p>
+                    <p class="text-xl text-gray-600">任務統計</p>
+                    <p class="text-2xl text-gray-900" id="task_nums">? 件</p>
                 </div>
-            </div>
-            <div class="mx-4 my-2 px-5">
-                <a href="" class="mt-2 text-green-700">查看資訊</a>
             </div>
         </div>
         <!--數據欄位-->
         <div class="rounded m-2 flex-1 bg-gray-50 p-4">
-            <div class="flex items-center mx-4 px-5 my-2">
+            <div class="flex items-center xl:mx-4 lg:px-5 my-2 content-around">
                 <div class="flex grid">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         class="h-8 w-8 justify-self-end text-gray-600" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 justify-self-end text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
                     </svg>
                 </div>
                 <div class="mx-4 px-5">
-                    <p class="text-xl text-gray-600">標題</p>
-                    <p class="text-2xl text-gray-900">數據</p>
+                    <p class="text-xl text-gray-600">已收案任務</p>
+                    <p class="text-2xl text-gray-900" id="task_nums">? 件</p>
                 </div>
-            </div>
-
-            <div class="mx-4 my-2 px-5">
-                <a href="" class="mt-2 text-green-700">查看資訊</a>
             </div>
         </div>
     </div>
